6d79e23e127409c8542cbad4a399d5e7dab5333b
Brienne Kordis <87245718+briennekordis@users.noreply.github.com>
2021-11-09 16:41:40 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { render } from '@testing-library/react';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+
+function App() {
+  const [fact, setFact] = useState('');
+    const handleClick = async () => {
+      try {
+        await fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1')
+        .then(response => response.json())
+        .then(result => {
+          setFact(result.text)
+          render(result.text);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Click the button for a cat fact.</h1>
+        <button onClick={handleClick}>Fact, please!</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
